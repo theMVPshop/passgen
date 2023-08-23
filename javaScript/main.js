0d4fe4e35f2bbc0dc4689af4a6ab729470deed71
Caroline-Patrick <carolinespatrick@gmail.com>
2023-08-23 18:50:31 -0500

    changed ui to have scroll effect

--- javaScript/main.js
+++ javaScript/main.js
@@ -1,39 +1,79 @@
 // This can be a place to setup event listeners and be the main entry point that makes calls to other functions in other files.
+document.addEventListener('DOMContentLoaded', function() {
+    // When the page loads, we set up all the event listeners
 
+    // Event listener for "Get Started" button
+    document.querySelector('#btn-start').addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector('.password-generator').style.display = 'block';
+        document.querySelector('.password-generator').scrollIntoView({ behavior: 'smooth' });
+    });
 
-// event listener for "Get Started" button
-document.querySelector('#btn-start').addEventListener('click', function() {
-    document.querySelector('.hero').style.display = 'none';
-    document.querySelector('.password-generator').style.display = 'block';
+    // Event listener for "Generate Password" button
+    document.querySelector('#btn-generate').addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector('.password-show').style.display = 'block';
+        document.querySelector('.password-show').scrollIntoView({ behavior: 'smooth' });
+    });
+
+    // Event listener to bring to password check section
+    document.querySelector('#btn-pw-check').addEventListener('click', function(event) {
+        event.preventDefault();
+        document.querySelector('.password-check').style.display = 'block';
+        document.querySelector('.password-check').scrollIntoView({ behavior: 'smooth' });
+    });
+
+    // Event listener to show pw hint
+    document.querySelector('#btn-reveal-hint').addEventListener('click', function() {
+        document.querySelector('.hint-phrase').style.display = 'block';
+        document.querySelector('#btn-reveal-hint').style.display = 'none';
+        document.querySelector('#btn-hide-hint').style.display = 'block';
+    });
+
+    // Event listener to hide pw hint
+    document.querySelector('#btn-hide-hint').addEventListener('click', function() {
+        document.querySelector('.hint-phrase').style.display = 'none';
+        document.querySelector('#btn-hide-hint').style.display = 'none';
+        document.querySelector('#btn-reveal-hint').style.display = 'block';
+    });
 });
 
-//event listener for generate pw
-document.querySelector('#btn-generate').addEventListener('click', function() {
-    document.querySelector('.password-generator').style.display = 'none';
-    document.querySelector('.password-show').style.display = 'block';
-});
 
-//event listener to bring to pw check section
-document.querySelector('#btn-pw-check').addEventListener('click', function() {
-    document.querySelector('.password-show').style.display = 'none';
-    document.querySelector('.password-check').style.display = 'block';
-    document.querySelector('#btn-hide-hint').style.display = 'none';
+// // event listener for "Get Started" button
+// document.querySelector('#btn-start').addEventListener('click', function(event) {
+//     event.preventDefault();
+//     // document.querySelector('.hero').style.display = 'blo';
+//     document.querySelector('.password-generator').style.display = 'block';
+// });
 
-});
+// //event listener for generate pw
+// document.querySelector('#btn-generate').addEventListener('click', function(event) {
+//     event.preventDefault();
+//     document.querySelector('.password-generator').style.display = 'none';
+//     document.querySelector('.password-show').style.display = 'block';
+// });
 
-// event listener to show pw hint
-document.querySelector('#btn-reveal-hint').addEventListener('click', function() {
-    document.querySelector('.hint-phrase').style.display = 'block';
-    document.querySelector('#btn-reveal-hint').style.display = 'none';
-    document.querySelector('#btn-hide-hint').style.display = 'block'; 
-});
+// //event listener to bring to pw check section
+// document.querySelector('#btn-pw-check').addEventListener('click', function(event) {
+//     event.preventDefault();
+//     document.querySelector('.password-show').style.display = 'none';
+//     document.querySelector('.password-check').style.display = 'block';
+// });
 
-// event listener to hide pw hint
-document.querySelector('#btn-hide-hint').addEventListener('click', function() {
-    document.querySelector('.hint-phrase').style.display = 'none';
-    document.querySelector('#btn-hide-hint').style.display = 'none';
-    document.querySelector('#btn-reveal-hint').style.display = 'block';
-});
+
+// // event listener to show pw hint
+// document.querySelector('#btn-reveal-hint').addEventListener('click', function() {
+//     document.querySelector('.hint-phrase').style.display = 'block';
+//     document.querySelector('#btn-reveal-hint').style.display = 'none';
+//     document.querySelector('#btn-hide-hint').style.display = 'block'; 
+// });
+
+// // event listener to hide pw hint
+// document.querySelector('#btn-hide-hint').addEventListener('click', function() {
+//     document.querySelector('.hint-phrase').style.display = 'none';
+//     document.querySelector('#btn-hide-hint').style.display = 'none';
+//     document.querySelector('#btn-reveal-hint').style.display = 'block';
+// });
 
 function generateRandomPassword(animals, clothes, colors, length) {
     let password = "";
