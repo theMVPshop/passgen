3811c138aca7d8be9e5a40420a2e5d43ec3c92bc
SamSvetlik <sam.svetlik@gmail.com>
2023-08-22 19:45:08 -0500

    was unclear on what Nathan and Edward had been working on, so I created arrays for the prompt options and functions to generate phrases and prompts. Might be redundant

---
+++ data/celebrities.js
@@ -0,0 +1,14 @@
+const celebrities = [
+    "Barack Obama", 
+    "Taylor Swift", 
+    "Dwayne Johnson", 
+    "Lebron James", 
+    "Oprah Winfrey", 
+    "Ellen DeGeneres", 
+    "Donald Trump", 
+    "Elon Musk", 
+    "Mark Zuckerberg", 
+    "Leonardo DiCaprio"
+]
+
+export default celebrities
